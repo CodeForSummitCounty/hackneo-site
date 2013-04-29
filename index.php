6657479f39e62c49b47698e9dc0db47fe976d77c
Persa Zula <persa@persazula.com>
2013-04-28 20:33:04 -0400

    Initial website design

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>HackNEO | Hack for Northeast Ohio on June 1st and 2nd | Akron, Ohio</title>
+        <meta name="description" content="Join developers, designers, and community members in Akron, Ohio to build a mobile mapping application for the parks!">
+        <meta name="viewport" content="width=device-width">
+
+        <link href='http://fonts.googleapis.com/css?family=Ubuntu:400,500,700,400italic' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link href="css/structure.css" rel="stylesheet">
+        <link href="css/form.css" rel="stylesheet">
+        <link href="css/theme.css" rel="stylesheet">
+
+
+        <script src="js/vendor/wufoo.js"></script>
+
+
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+    <div class="wrapper clearfix">    
+        <div class="left">
+            <header>
+                <img src="img/hackneo_logo.png" alt="Hack Northeast Ohio June 1st and 2nd, 2013">
+            </header>
+            <article>
+                <h2 class="green">Developers, designers, writers, community members, computer wizards, park lovers, and data geeks:</h2>
+                <p class="block white">Join us for this free event on the National Day of Civic Hacking and help us use open data to build a mapping app for our regional parks system!</p>
+                <p class="block white">Breakfast and lunch provided. All participants receive an event tshirt and other goodies</p>
+                <p class="time block green">June 1st &amp; 2nd, 2013<br />9:30 am - 4:00pm</p>
+                <p class="address block blue">Summit ArtSpace<br />140 East Market Street<br />Akron, OH 44308</p>
+            </article>
+        </div>
+
+        <div class="right">
+<!--             <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">What is Civic Hacking?</a></li>
+                    <li><a href="#">Become a Sponsor</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav> -->
+
+            <section class="content">
+                <?php include "include/form.html"; ?>
+            </section>
+        </div>
+    </div>
+
+        <section class="wrapper sponsorship">
+            <h2>Thanks to our sponsors:</h2>
+            <a href="http://www.codeforsummitcounty.org" target="_blank"><img src="img/cfsc_logo.png" class="sponsor" alt="Code for Summit County">
+            </a>
+            <a href="http://www.akronareaarts.org/summit-artspace/about-summit-artspace/" target="_blank"><img src="img/summit-artspace.png" class="sponsor" alt="Summit ArtSpace">
+            </a>
+            <a href="http://www.codeforamerica.org" target="_blank"><img src="img/CfA_logo_greyscale.png" class="sponsor" alt="Code for America">
+            </a>
+            <a href="http://www.hackforchange.org" target="_blank"><img src="img/logo_ndoch_black.png" class="sponsor" alt="Hack for Change">
+            </a>
+            <a href="http://www.cartodb.com" target="_blank"><img src="img/logos_full_cartodb_light.png" class="sponsor" alt="CartoDB">
+            </a>
+            <a href="http://www.oreilly.com" target="_blank"><img src="img/oralogo_bkgy.png" class="sponsor" alt="O'Reilly Media">
+            </a>
+        </section>
+
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+
+        <script src="js/main.js"></script>
+
+        <script>
+            var _gaq=[['_setAccount','UA-39250227-3'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+    </body>
+</html>
---
+++ thanks.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>HackNEO | Hack for Northeast Ohio on June 1st and 2nd | Akron, Ohio</title>
+        <meta name="description" content="Join developers, designers, and community members in Akron, Ohio to build a mobile mapping application for the parks!">
+        <meta name="viewport" content="width=device-width">
+
+        <link href='http://fonts.googleapis.com/css?family=Ubuntu:400,500,700,400italic' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+
+        <script type="text/javascript">var switchTo5x=true;</script>
+<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+<script type="text/javascript">stLight.options({publisher: "7a9170d3-5ef5-4bfc-8b42-91aa0b9904a4", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
+
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+    <div class="wrapper clearfix">    
+        <div class="left">
+            <header>
+                <img src="img/hackneo_logo.png" alt="Hack Northeast Ohio June 1st and 2nd, 2013">
+            </header>
+
+        </div>
+
+        <div class="right">
+<!--             <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">What is Civic Hacking?</a></li>
+                    <li><a href="#">Become a Sponsor</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav> -->
+
+            <section class="content">
+                <h2>
+                    Thank you for signing up for HackNEO!
+                </h2>
+                <p>Thank you for signing up for HackNEO! As our event date approaches, 
+                    we will email you with more information regarding the event, 
+                    including how to get here, where to park, and any other relevant news! 
+                    Remember to follow <a href="http://twitter.com/hack4NEO" target="_blank">@hack4NEO</a> on Twitter for more regular updates. 
+                    Please share HackNEO with anyone you think would make a 
+                    great addition to the event. Below are a few social networks you can share easily on: </p>
+
+                <span class='st_googleplus_hcount' displayText='Google +'></span>
+                <span class='st_facebook_hcount' displayText='Facebook'></span>
+                <span class='st_twitter_hcount' displayText='Tweet'></span>
+                <span class='st_linkedin_hcount' displayText='LinkedIn'></span>
+                <span class='st_pinterest_hcount' displayText='Pinterest'></span>
+                <span class='st_email_hcount' displayText='Email'></span>
+
+                <br>
+
+            </section>
+        </div>
+    </div>
+
+        <section class="wide sponsorship">
+            
+        </section>
+
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+
+        <script src="js/main.js"></script>
+
+        <script>
+            var _gaq=[['_setAccount','UA-39250227-3'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+    </body>
+</html>
